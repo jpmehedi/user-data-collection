bfd4280001ac1e0fde2839cca0bd2113f0ea039f
Mehedi Hasan <mehedi@augnitive.com>
2021-12-28 10:43:05 +0600

    Sign in and image picker

--- lib/screens/welcome/login.dart
+++ lib/screens/welcome/login.dart
@@ -1,26 +1,61 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:test_me/screens/home.dart';
 import 'package:test_me/utils/app_color.dart';
 import 'package:test_me/widgets/custom_back_button.dart';
 import 'package:test_me/widgets/custom_button.dart';
 import 'package:test_me/widgets/custom_textfield.dart';
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({ Key? key }) : super(key: key);
 
   @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool isLoading = false;
+  
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  Future signIn()async{
+    setState(() {
+      isLoading = true;
+    });
+    try{
+      UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: emailController.text,
+        password: passwordController.text
+      );
+      if(userCredential.user != null){
+        Route route = MaterialPageRoute(builder: (ctx)=> HomeScreen());
+        Navigator.push(context, route);
+      }
+    }catch(e){
+      print("Error: $e");
+    }
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+
+
+  @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          elevation: 0,
-          backgroundColor: AppColor.primaryColor,
-          leadingWidth: 100,
-          leading: CustomBackButton(
-            onTap: (){
-              Navigator.pop(context);
-            },
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: AppColor.primaryColor,
+        leadingWidth: 100,
+        leading: CustomBackButton(
+          onTap: (){
+            Navigator.pop(context);
+          },
         ),
-        body: Padding(
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
@@ -44,22 +79,26 @@ class LoginScreen extends StatelessWidget {
               ),
               SizedBox(height: 102,),
               CustomTextField(
+                controller: emailController,
                 hintText: "Email",
               ),
               SizedBox(height: 12,),
               CustomTextField(
+                controller: passwordController,
                 obscureText: true,
                 hintText: "Password",
                 suffixIcon: Icon(Icons.visibility),
               ),
               SizedBox(height: 59,),
-              CustomButton(
-                onTap: (){},
+              isLoading? Center(child: CircularProgressIndicator()) : CustomButton(
+                onTap: (){
+                  signIn();
+                },
                 color: AppColor.secondaryColor,
                 levelColor: AppColor.primaryColor,
                 buttonLevel: "Login",
               ),
-              Spacer(),
+              // Spacer(),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
--- lib/widgets/custom_back_button.dart
+++ lib/widgets/custom_back_button.dart
@@ -10,7 +10,7 @@ class CustomBackButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: (){},
+      onTap: onTap,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
